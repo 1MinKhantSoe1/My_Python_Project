a27229d551cc67849b20d5fd17cecac4ffb8eaf3
1MinKhantSoe1 <minkhantsoe1999@gmail.com>
2021-07-22 04:52:40 +0800

    Update

--- Convert_Text_Into_Audio.py
+++ Convert_Text_Into_Audio.py
@@ -1,7 +1,7 @@
 from gtts import gTTS
 from playsound import playsound
 
-audio = 'speech.mp3'
+audio = 'speech.mp3'  # output file name
 language = 'en'
 text = input("Please enter a speech that you want to convert into audio : ")
 sp = gTTS(text=text, lang=language, slow=False)

9b89ef5913b07671df0e8b3990ff72fe5260b40f
Min Khant Soe <63970830+1MinKhantSoe1@users.noreply.github.com>
2021-10-14 23:33:50 +0800

    Update WiFi_Password_Recovery.py

--- WiFi_Password_Recovery.py
+++ WiFi_Password_Recovery.py
@@ -2,7 +2,6 @@ import subprocess
 
 
 def main():
-
     print("WiFi Password Recovery")
     print("----------------------")
 
@@ -11,8 +10,11 @@ def main():
     data = subprocess.check_output(['netsh', 'wlan', 'show', 'profiles']).decode('utf-8').split('\n')
     profiles = [i.split(":")[1][1:-1] for i in data if "All User Profile" in i]
 
+    print("    Wifi Name \t\t\t Password ")
+    print("-" * 20, "\t\t-----------------")
     for i in profiles:
-        results = subprocess.check_output(['netsh', 'wlan', 'show', 'profile', i, 'key=clear']).decode('utf-8').split(
+        results = subprocess.check_output(['netsh', 'wlan', 'show', 'profile', i, 'key=clear']).decode(
+            'ISO-8859-1').split(
             '\n')
         results = [b.split(":")[1][1:-1] for b in results if "Key Content" in b]
         try:
@@ -20,12 +22,13 @@ def main():
         except IndexError:
             print("{:<30}|  {:<}".format(i, ""))
 
+
+
+if __name__ == '__main__':
+    main()
+
     print("")
 
     print("Created By Min Khant Soe (HakHak)")
 
     input("")
-
-
-if __name__ == '__main__':
-    main()
